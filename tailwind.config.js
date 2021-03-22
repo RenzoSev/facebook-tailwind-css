9a93885e4f2240f1dea84f78a2181c3b5cf25266
Renzo Sevilha <sevilharenzo@gmail.com>
2021-03-21 23:34:26 -0300

    [EXTEND] [color apple]

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        apple: {
+          DEFAULT: '#42B72A',
+          50: '#D8F5D2',
+          100: '#C6F0BD',
+          200: '#A2E694',
+          300: '#7EDD6A',
+          400: '#5AD341',
+          500: '#42B72A',
+          600: '#338E20',
+          700: '#246417',
+          800: '#153B0D',
+          900: '#061104'
+        }
+      }
+    },
   },
   variants: {
     extend: {},
